5ba7b64cc27a4e1a1661080470e3cf5ea042d3e8
r-ofarrell <ryanof@pm.me>
2025-01-16 07:46:48 -0500

    Created models: Credential, Insurance, and Therapist.

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Credential(models.Model):
+    name = models.CharField(max_length=75)
+
+
+class Insurance(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Therapist(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    license_type = models.CharField(max_length=50)
+    active = models.BooleanField(default=True)
+    credentials = models.ManyToManyField(Credential)
+    insurances = models.ManyToManyField(Insurance)
