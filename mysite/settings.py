b20c492a239bb5b5d5de39514b3d6b03826d049a
Ryan O'Farrell <ryanof@pm.me>
2025-04-12 19:35:04 -0400

    Added static files for deployment.

--- mysite/settings.py
+++ mysite/settings.py
@@ -35,6 +35,10 @@ DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.fly.dev']
 CSRF_TRUSTED_ORIGINS = ['https://*.fly.dev']
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+SECURE_SSL_REDIRECT = True
 
 # Application definition
 
